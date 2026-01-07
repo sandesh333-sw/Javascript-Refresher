410666d5923cd0f0f51ad894b50df52091405707
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-07 03:23:31 +0000

    feat: data types notes

---
+++ part1/datatype.js
@@ -0,0 +1,16 @@
+// How to write comments 
+//Either use // or /* */
+/*
+Data types:
+String: sita , ram
+Number: any numbers
+Boolean: yes or no
+Bigint: Bigger numbers
+
+Undefined: means has no definition yet, i know you exist but may be available in future or maybe not
+null: empty but not zero
+
+Object: includes things like arrays, dictonaries
+
+Symbol: If you wanna make anything uniquethat is where its used
+ */
